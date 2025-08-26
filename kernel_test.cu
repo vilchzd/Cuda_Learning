8a077f7dede2c6f74ff0fd2e278d7132b7f4605b
vilchez <dievilchez23@gmail.com>
2025-08-26 02:53:20 -0600

    Implement GPU increment kernel and measure performance

--- kernel_test.cu
+++ kernel_test.cu
@@ -2,57 +2,79 @@
 #include <cstdlib>   
 #include <iomanip>
 #include <cuda_runtime.h>
+#include <chrono>
+using namespace std::chrono;
 using namespace std;
 
-// CUDA kernel: runs on GPU
-__global__ void kernel(int *d_in, int *d_out) {
-    printf("Changing contents of host pointer!\n");
-    *d_out = *d_in;
+
+/* 
+    //Allocate mem on device (LOCATION, SIZE); address, number of bytes to allocate | Allocating memory on the GPU, and storing the ADD of that GPU memory into host pointer d_c.
+    cudaMalloc((void**)&d_c, sizeof(int));
+
+    //Args (dst, src, numBytes, direction) , (point2addcopyto,point2addcopyfrom, N*sizeof(type), cudaMemcpyHosttoDevice/DevicetoHost)
+    cudaMemcpy(d_c, h_c, sizeof(int), cudaMemcpyHostToDevice);
+ */
+
+
+
+__global__ void increment_gpu(int *a, int N) {
+    int i = threadIdx.x;
+    if (i < N) {
+        a[i] = a[i] + 1;
+    }
 }
 
+void increment_cpu(int *a, int N) {
+    for (int i=0; i<N; i++) {
+        if (i < N) {
+            a[i] = a[i] + 1;
+        }
+    }
+}
 
 
 int main() {
     cudaDeviceProp prop;
     cudaGetDeviceProperties(&prop, 0);
     cout << "Using device: "<< prop.name << endl;
+    // ---------------------------------------------------------------------------//
+    int N = 1000;
+    int *h_a = (int*)malloc(N * sizeof(int));
+    int *d_a = nullptr;
+    for (int i = 0; i < N; i++) h_a[i] = 0;
+    cudaMalloc((void**)&d_a, N*sizeof(int));
+    cudaMemcpy(d_a, h_a, N*sizeof(int), cudaMemcpyHostToDevice);
+    dim3 grid_size(1);
+    dim3 block_size(N);
 
+    auto start = high_resolution_clock::now();
+    increment_cpu(h_a, N);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Time elapsed: " << duration.count() << " us" << endl;
 
-    //Host and Device pointer variables that hold memory addresses
-    int *h_c, *d_c;
-    //Allocate mem on host
-    h_c = (int*)malloc(sizeof(int));
-    *h_c = 200; 
-    //Allocate mem on device (LOCATION, SIZE); address, number of bytes to allocate | Allocating memory on the GPU, and storing the ADD of that GPU memory into host pointer d_c.
-    cudaMalloc((void**)&d_c, sizeof(int));
-
-    //Copy data from host to device
-    //Args (dst, src, numBytes, direction) , (point2addcopyto,point2addcopyfrom,N*sizeof(type), cudaMemcpyHosttoDevice/DevicetoHost)
-    cudaMemcpy(d_c, h_c, sizeof(int), cudaMemcpyHostToDevice);
-    cout << "Host pointer value before kernel launch: " << *h_c << endl;
+    cudaEvent_t gpu_start, gpu_stop;
+    cudaEventCreate(&gpu_start);
+    cudaEventCreate(&gpu_stop);
+    cudaEventRecord(gpu_start);
     
-    //Launch kernel
-    //dim3 grid_size(1,1);
-    //dim3 block_size(1,1);
-    //kernel<<<grid_size, block_size>>>();
+    increment_gpu<<<grid_size, block_size>>>(d_a, N);
 
+    cudaEventRecord(gpu_stop);
+    cudaEventSynchronize(gpu_stop);
 
+    cudaMemcpy(d_a, h_a, N*sizeof(int), cudaMemcpyDeviceToHost);
+    for (int i=0; i < N; i++) {
+        cout << "a[" << i << "] = " << h_a[i] << endl;
+    }
 
-    // Wait for GPU to finish
-    cudaDeviceSynchronize();
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, gpu_start, gpu_stop);
+    cout << "GPU time: " << milliseconds * 1000 << " us" << endl;
 
-
-
-    //Copy data from device to host
-    cudaMemcpy(h_c, d_c, sizeof(int), cudaMemcpyDeviceToHost);
-
-    // Print from CPU
-    cout << "Host pointer address: 0x" << hex << reinterpret_cast<uintptr_t>(h_c) << endl;
-    cout << "Host pointer value after kernel launch: " << dec << *h_c << endl;
-
-
-    cudaFree(d_c);
-    free(h_c);
-        
+    free(h_a);
+    cudaFree(d_a); 
+    cudaEventDestroy(gpu_start);
+    cudaEventDestroy(gpu_stop);
     return 0;
 } 
