aeb815ba8ac5abc7b49bd50c86004372462dca4b
vilchez <dievilchez23@gmail.com>
2025-08-28 14:54:56 -0600

    update memory_model.cu for scalebility

--- memory_model.cu
+++ memory_model.cu
@@ -4,12 +4,10 @@
 #include <iomanip>
 #include <cuda_runtime.h>
 
-#define N 3
-#define BLOCK_SIZE 3
+#define N 256
+#define BLOCK_SIZE 16
 
 using namespace std;
-
-
 
 __global__ void transpose(float *in, float *out, int width) {
     __shared__ float t_mat[BLOCK_SIZE][BLOCK_SIZE];
@@ -35,11 +33,11 @@ __global__ void transpose(float *in, float *out, int width) {
 
 
 int main() {
+
     int width = N;
     float *d_in, *d_out;
     float *h_in = (float*)malloc(width*width*sizeof(float));
     float *h_out = (float*)malloc(width*width*sizeof(float));
-    
 
     for (int i = 0; i < width*width; i++) {
         h_in[i] = i+1;
@@ -52,9 +50,20 @@ int main() {
     
     dim3 block_size(BLOCK_SIZE, BLOCK_SIZE);
     dim3 grid_size((N + BLOCK_SIZE - 1)/BLOCK_SIZE, (N + BLOCK_SIZE - 1)/BLOCK_SIZE);
+
+      
+    cudaEvent_t gpu_start, gpu_stop;
+    cudaEventCreate(&gpu_start);
+    cudaEventCreate(&gpu_stop);
+    cudaEventRecord(gpu_start);
+
     transpose<<<grid_size, block_size>>>(d_in, d_out, N);
     cudaDeviceSynchronize();
     
+    
+    cudaEventRecord(gpu_stop);
+    cudaEventSynchronize(gpu_stop);
+
     cudaMemcpy(h_out, d_out, width*width*sizeof(float), cudaMemcpyDeviceToHost);
 
     for(int i=0;i<width;i++){
@@ -62,6 +71,12 @@ int main() {
             cout << h_out[i*width+j] << " ";
         cout << endl;
     }
+
+
+    float milliseconds = 0;
+    cudaEventElapsedTime(&milliseconds, gpu_start, gpu_stop);
+    cout << "GPU time: " << milliseconds << " ms" << endl;
+
 
     free(h_in);
     free(h_out);
