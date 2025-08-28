d850de5da290833171e9468af367c0106fc6c941
vilchez <dievilchez23@gmail.com>
2025-08-27 18:13:52 -0600

    add matrix transposition using shared memory

---
+++ memory_model.cu
@@ -0,0 +1,71 @@
+// int i = threadIdx.x + blockDim.x * blockIdx.x
+#include <iostream>   
+#include <cstdlib>   
+#include <iomanip>
+#include <cuda_runtime.h>
+
+#define N 3
+#define BLOCK_SIZE 3
+
+using namespace std;
+
+
+
+__global__ void transpose(float *in, float *out, int width) {
+    __shared__ float t_mat[BLOCK_SIZE][BLOCK_SIZE];
+
+    int i = threadIdx.x + BLOCK_SIZE * blockIdx.x ;
+    int j = threadIdx.y + BLOCK_SIZE * blockIdx.y ;
+    
+
+    if (i < width && j < width) {
+        t_mat[threadIdx.x][threadIdx.y] = in[i * width + j];
+    }
+
+    __syncthreads();
+
+    int trans_i = threadIdx.y + BLOCK_SIZE * blockIdx.x;
+    int trans_j = threadIdx.x + BLOCK_SIZE * blockIdx.y;
+
+
+    if (trans_i < width && trans_j < width) {
+        out[trans_i * width + trans_j] = t_mat[threadIdx.x][threadIdx.y];
+    }
+}
+
+
+int main() {
+    int width = N;
+    float *d_in, *d_out;
+    float *h_in = (float*)malloc(width*width*sizeof(float));
+    float *h_out = (float*)malloc(width*width*sizeof(float));
+    
+
+    for (int i = 0; i < width*width; i++) {
+        h_in[i] = i+1;
+    }
+
+    cudaMalloc((void**)&d_in, width*width*sizeof(float));
+    cudaMemcpy(d_in, h_in, width*width*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMalloc((void**)&d_out, width*width*sizeof(float));
+    
+    
+    dim3 block_size(BLOCK_SIZE, BLOCK_SIZE);
+    dim3 grid_size((N + BLOCK_SIZE - 1)/BLOCK_SIZE, (N + BLOCK_SIZE - 1)/BLOCK_SIZE);
+    transpose<<<grid_size, block_size>>>(d_in, d_out, N);
+    cudaDeviceSynchronize();
+    
+    cudaMemcpy(h_out, d_out, width*width*sizeof(float), cudaMemcpyDeviceToHost);
+
+    for(int i=0;i<width;i++){
+        for(int j=0;j<width;j++)
+            cout << h_out[i*width+j] << " ";
+        cout << endl;
+    }
+
+    free(h_in);
+    free(h_out);
+    cudaFree(d_in);
+    cudaFree(d_out);
+    return 0;
+} 
